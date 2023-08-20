3127339c1a271a2303fb1cba20080703c2cbff83
MD MOSTAFIZUR RAHMAN ZAHID <88180630+mostafizur-zahid@users.noreply.github.com>
2023-08-20 22:08:51 +0600

    first commit

---
+++ Algorithm/1.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int n;
+    cin>>n;
+    for(int i=0;i<n;i++){
+        int m;
+        cin>>m;
+        int array[m];
+        for(int j=0;j<m;j++){
+            cin>>array[j];
+        }
+        int max = array[0];
+        for(int k=0;k<m;k++){
+            if(max<array[k])
+            max=array[k];
+        }
+        cout<<"case "<<i<<":"<<max<<endl;
+    }
+
+}
